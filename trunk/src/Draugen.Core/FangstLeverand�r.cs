d2c889860501ce97031acf242982fc98abd2323e
eriksen <simen.eriksen@draugen.org>
2010-08-13 01:09:27 +0200

    update domain model

--- trunk/src/Draugen.Core/FangstLeverand�r.cs
+++ trunk/src/Draugen.Core/FangstLeverand�r.cs
@@ -4,6 +4,11 @@ namespace Draugen
 {
     public abstract class FangstLeverandør : Kommenterbar
     {
+        protected FangstLeverandør()
+        {
+            Fangster = new List<Fangst>();    
+        }
+
         public virtual IList<Fangst> Fangster { get; set; }
 
         public virtual string Navn { get; set; }
--- trunk/src/Draugen.Core/Kommenterbar.cs
+++ trunk/src/Draugen.Core/Kommenterbar.cs
@@ -4,6 +4,11 @@ namespace Draugen
 {
     public abstract class Kommenterbar : DomainObject
     {
+        protected Kommenterbar()
+        {
+            Kommentarer = new List<Kommentar>();
+        }
+
         public virtual IList<Kommentar> Kommentarer { get; set; }
     }
 }
